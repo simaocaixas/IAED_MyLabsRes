805e12cb674d1e716b12aac889a75f6712e2f143
Simão Caixas <145067642+simaocaixas@users.noreply.github.com>
2024-02-26 22:39:56 +0000

    Add files via upload

---
+++ Lab04/ex05.c
@@ -0,0 +1,21 @@
+/* iaed24 - ist1109632 - lab04/ex01 */
+#include <stdio.h>
+#define LEN 100
+
+int leLinha(char s[]) {
+    int i = 0;
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+int main() {
+    char linha[LEN];
+    leLinha(linha);
+    printf("%s\n", linha);
+
+    return 0;
+}
