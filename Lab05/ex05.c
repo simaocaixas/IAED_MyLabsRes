41198c58d32dda8fa4310e387119ded257f68b27
Simão Caixas <145067642+simaocaixas@users.noreply.github.com>
2024-02-29 09:26:40 +0000

    Update ex05.c

--- Lab05/ex05.c
+++ Lab05/ex05.c
@@ -18,14 +18,11 @@
         soma_horas = relogio1.horas + relogio2.horas;
         soma_minutos = relogio1.minutos + relogio2.minutos;
 
-    
-
         if (soma_minutos >= 60) {
             soma_horas += (soma_minutos / 60);
             soma_minutos -= (soma_minutos / 60) * 60;
 
         }
-
         printf("%02d:%02d\n", soma_horas, soma_minutos);
         return 0;
     }
