142d0f36187ea1f53b7345574d0fbe1c0979d809
Simão Caixas <145067642+simaocaixas@users.noreply.github.com>
2024-03-04 09:50:45 +0000

    Update and rename ex02.c to ex02-ex03.c

--- Lab06/ex02-ex03.c
+++ Lab06/ex02-ex03.c
@@ -14,7 +14,23 @@ int search(int a[], int n, int elem) {    // Avaliar a complexidade da função 
 }                                       
 
 /*
-O tamanho do intervalo onde estamos a procurar o elemento é constantemente reduzido a metade logo 
+R: O tamanho do intervalo onde estamos a procurar o elemento é constantemente reduzido a metade logo 
 podemos considerar um algoritmo de busca binario de compelexidade O(LogN), onde N é o n de elementos 
-do nosso vetor!
+do nosso vetor.
+*/ 
+
+
+------------------------------------------------------------------------------------------------------------------------------
+
+int funcao (int a[], int n) {       // Avaliar a complexidade da função 'funçaõ' com notação big O; 
+  int i, sum = 0;                  
+
+  for (i = 0; i < n/2; i++)        // Itera n/2 vezes o vetor, ou seja se a = [1,2,3,4,5,6] itera 3 vezes
+    sum += a[i] + a[n-i-1];        // sum = sum + 1 + 6 depois sum = sum + 2 + 5 depois sum = sum + 3 + 4 ...
+  return sum;                       
+}
+
+/*
+R: No melhor caso e no pior caso a funcao tem a mesma complexidade, ou seja itera N/2 vezes o vetor, logo 
+o crescimento da complexidade é constante: O(N).
 */ 
