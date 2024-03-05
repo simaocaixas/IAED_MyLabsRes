c83f70dbadd857ec3392260aa14f8b99157324f0
Simão Caixas <145067642+simaocaixas@users.noreply.github.com>
2024-03-05 11:29:38 +0000

    Update ex02-ex03-ex04-ex05.c

--- Lab06/ex02-ex03-ex04-ex05.c
+++ Lab06/ex02-ex03-ex04-ex05.c
@@ -66,7 +66,7 @@ int funcao (int a[], int n, int b[], int m) {      // n e m são os tamanhos de 
       if (a[i] == b[j])
         count++;
     }
-    i = i/2;                                       // O vetor a é iterado é utilizado busca binaria logo complexidade O(LogN);
+    i = i/2;                                       // Complexidade O(LogN);
   }
   return count;
 }
